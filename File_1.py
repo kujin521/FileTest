a6d3de734fabdf58a086b75376d39022801066f1
kujin <1604323361@qq.com>
2020-10-05 18:58:32 +0800

    增加了注释,便于理解

--- File_1.py
+++ File_1.py
@@ -2,7 +2,15 @@ import os # 读取文件路径
 import io # 输入输出
 
 root = 'D:\OneDrive\hexo\source\_posts\随心记'  # 你的文章地址
-#root = '测试文件夹'  # 本地测试
+# root = '测试文件夹'  # 本地测试
+
+# 读取文件内容并返回
+def readFileContent(str):
+    s=""
+    for line in open(str,"r",encoding="utf-8", errors='ignore'):
+        s+=line
+    return s
+str_modle=readFileContent('modle.md')
 
 # 批量替换
 def batch_replace(file, content):
@@ -21,14 +29,12 @@ for root, subFolder, filenames in os.walk(root):
         # 以 .md 结尾的文件
         if os.path.splitext(filename)[1] == '.md':
             sl=root.split("\\")
-            content = """---
-title: {} # 标题=文件名
-auther: ku jin # 作者
-categories: # 分类=根文件夹名
- - {}
-tags: # 标签=目标文件夹
- - {} 
----""".format(os.path.splitext(filename)[0], sl[6], os.path.splitext(filename)[0])+"\n"
+            # 要动态修改模板内容
+            content = str_modle.format(os.path.splitext(filename)[0], # 获取文件名替换标题
+              # str(list(sl)[6:]),   # 为文章添加多个分类，可以尝试以下 list 中的方法 下标 我这里是 6 开始截取
+              sl[6],   # 为文章添加多个分类，可以尝试以下 list 中的方法 下标 我这里是 6 开始截取
+              #os.path.splitext(filename)[0])+"\n"
+              sl[6])+"\n"
             # 批量添加文件开头yaml
             batch_replace(filename, content)
             print("{} 替换完成".format(filename))
---
+++ demo_test1.py
@@ -0,0 +1,33 @@
+import os  # 读取文件路径
+import io  # 输入输出
+
+root = 'D:\OneDrive\hexo\source\_posts\随心记'  # 你的文章地址
+
+
+# root = '测试文件夹'  # 本地测试
+
+
+# 批量替换
+def batch_replace(file, content):
+    # 读取文件内容
+    with io.open(os.path.join(root, file), "r", encoding="utf-8", errors='ignore') as f:
+        for line in f:
+            content += line
+    # 写入内容
+    with io.open(os.path.join(root, file), "w", encoding="utf-8", errors='ignore') as f:
+        f.write(content)
+
+
+# 遍历目录下文件
+for root, subFolder, filenames in os.walk(root):
+    # 遍历文件
+    for filename in filenames:
+        # 以 .md 结尾的文件
+        if os.path.splitext(filename)[1] == '.md':
+            sl = root.split("\\")  #
+            print(sl[6])
+            # title=os.path.splitext(filename)[0]  # 获取文件名替换标题
+            # list1=list(sl)[5:]
+            # # str="aa: "+list1
+            # print(str(list1),title)
+
